5fe33c7ee5a12fa4cb2062c382d9bba62a9cca43
AigulKh <haku.kavai@gmail.com>
2017-05-09 21:09:25 +0300

    rotated

--- 3dGraphics/scene.h
+++ 3dGraphics/scene.h
@@ -1,6 +1,7 @@
 #ifndef SCENE_H
 #define SCENE_H
 
+#include <QtOpenGL/QGLWidget>
 #include <QOpenGLWidget>
 #include <QMouseEvent>
 #include "coordaxes.h"
@@ -15,7 +16,7 @@
 #include "onesheetedhyperboloid.h"
 #include "twosheetedhyperboloid.h"
 
-class Scene : public QOpenGLWidget/*, public QOpenGLFunctions*/ {
+class Scene : public QGLWidget/*, public QOpenGLFunctions*/ {
 public:
     Scene( QWidget *parent = 0 );
     ~Scene();
@@ -25,9 +26,8 @@ public:
     void mousePressEvent(QMouseEvent* pe);   // нажатие на клавишу мыши
     void mouseMoveEvent(QMouseEvent* pe);    // перемещение мыши
     void mouseReleaseEvent(QMouseEvent* pe); // отжатие клавиши мыши
-    void wheelEvent(QWheelEvent *);          // вращение колесика
 
-    int xRotation,yRotation,zRotation,scale; // переменные поворота и масштаба
+    int xRotation, yRotation, zRotation, scale; // переменные поворота и масштаба
     QPoint mousePos; // переменная для запоминания позиции нажатия мышки
 };
 
